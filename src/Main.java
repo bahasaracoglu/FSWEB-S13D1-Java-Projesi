2a6ed2ee008902f81fd4bb53b8a05d01b97b4583
bahasaracoglu <bahasaracoglu@gmail.com>
2023-07-24 21:32:40 +0300

    shouldWakeUp

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
+        System.out.println(shouldWakeUp(true,1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
     }
-}
+
+    public static boolean shouldWakeUp (boolean isBarking, int hour) throws Exception {
+        if(!isBarking) return false;
+        if(hour > 23 || hour < 0) return false;
+        return (hour >= 20 || hour < 8);
+
+}}
+
